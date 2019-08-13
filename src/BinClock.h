e26d52cd8cf7260aa217bff2156cf0df4342bbdc
Humphrey Chiramba <chrhum001@myuct.ac.za>
2019-08-13 22:07:07 +0200

    FInished main header file

--- src/BinClock.h
+++ src/BinClock.h
@@ -24,9 +24,9 @@ const char HOUR = 0x02;
 const char TIMEZONE = 2; // +02H00 (RSA)
 
 // define pins
-const int LEDS[] = {0,2,3,25,7,22,21,27,4,6}; //H0-H4, M0-M5
-const int SECS = 1;
-const int BTNS[] = {5,30}; // B0, B1
+const int LEDS[] = {8,10,16,18,22,24,26,32,36,38}; //H0-H4, M0-M5
+const int SECS = 12;
+const int BTNS[] = {13,15}; // B0, B1
 
 
 #endif
